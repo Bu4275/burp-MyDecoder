bbe02b94b931c13519aa8a61551ac412e149e40f
bu4275 <bu4275@gmail.com>
2017-12-21 22:02:58 +0800

    init

---
+++ MyDecoder.py
@@ -0,0 +1,222 @@
+#!-*- coding:utf-8 -*-
+
+from burp import IBurpExtender
+from burp import IBurpExtenderCallbacks
+from burp import IContextMenuFactory
+from burp import IHttpRequestResponse
+from burp import IMessageEditorController
+from burp import IMessageEditorTabFactory
+from burp import ITab
+from burp import IMessageEditorTab
+from burp import IScannerCheck
+from burp import IScanIssue
+from burp import IExtensionStateListener
+from javax import swing
+from java.awt import Font, Color
+import re
+import sys
+import threading
+import time
+
+sysEncodingType = sys.getfilesystemencoding()
+
+class BurpExtender(IBurpExtender, IContextMenuFactory, ITab, IExtensionStateListener):
+    TEXTAREA_WIDTH = 650
+    TEXTAREA_HEIGHT = 150
+    LABEL_WIDTH = 100
+    LABEL_HEIGHT = 25
+
+    def registerExtenderCallbacks(self, callbacks):
+        self._helpers = callbacks.getHelpers()
+
+        self._jDecoderPanel = swing.JPanel()
+        self._jDecoderPanel.setLayout(None)
+
+        # Combobox Values
+        self._decodeType = ['Unicode to Str', 
+                            'UTF-8 to Str',
+                            'Str to Unicode',
+                            'Str To UTF-8']
+
+        self._decodeTypeFunc = [self.unicodeToStr, 
+                                self.utf8ToStr,
+                                self.strToUnicode,
+                                self.strToUtf8]
+
+        # GUI components
+        self._jLabelInput = swing.JLabel()
+        self._jLabelOutput = swing.JLabel()
+        self._jLabelExample = swing.JLabel()
+        self._jLabelOputFormat = swing.JLabel()
+        self._jCheckBoxOutputFormat = swing.JCheckBox()
+        self._jTextAreaInputData = swing.JTextArea()
+        self._jTextAreaOutputData = swing.JTextArea()
+        self._jScrollPaneIntput = swing.JScrollPane(self._jTextAreaInputData)
+        self._jScrollPaneOutput = swing.JScrollPane(self._jTextAreaOutputData)
+
+        self._jButtonDecoder = swing.JButton('Execute', actionPerformed=self.decode)
+        self._jComboDecodeType = swing.JComboBox(self._decodeType, actionListener=self.change_decode)
+
+        # Configure GUI
+        self._jLabelInput.setText('Input:')
+        self._jLabelOutput.setText('Output:')
+        self._jLabelExample.setText('Example: ')
+        self._jLabelOputFormat.setText(r'Replace % with \ ')
+        self._jLabelExample.setFont(Font("Consolas", Font.PLAIN, 14))
+
+        self._jDecoderPanel.add(self._jLabelInput)
+        self._jDecoderPanel.add(self._jLabelOutput)
+
+        self._jScrollPaneIntput.setVerticalScrollBarPolicy(swing.JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED)
+        self._jScrollPaneOutput.setVerticalScrollBarPolicy(swing.JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED)
+        self._jLabelExample.setText(self._decodeTypeFunc[0]())
+
+
+        # Configure locations
+        self._jLabelInput.setBounds(20, 15, self.LABEL_WIDTH, self.LABEL_HEIGHT)
+        self._jLabelOutput.setBounds(20, 225, self.LABEL_WIDTH, self.LABEL_HEIGHT)
+        self._jLabelExample.setBounds(20, 190, self.TEXTAREA_WIDTH, 30)
+        self._jLabelOputFormat.setBounds(self.TEXTAREA_WIDTH + 80, 117, 150, 30)
+        self._jCheckBoxOutputFormat.setBounds(self.TEXTAREA_WIDTH + 50, 120, 20, 20)
+        self._jScrollPaneIntput.setBounds(20 ,40, self.TEXTAREA_WIDTH, self.TEXTAREA_HEIGHT)
+        self._jScrollPaneOutput.setBounds(20, 250, self.TEXTAREA_WIDTH, self.TEXTAREA_HEIGHT)
+        self._jButtonDecoder.setBounds(self.TEXTAREA_WIDTH + 50, 40, 150, 30)
+        self._jComboDecodeType.setBounds(self.TEXTAREA_WIDTH + 50, 80, 150,30)
+ 
+        self._jDecoderPanel.add(self._jLabelInput)
+        self._jDecoderPanel.add(self._jLabelOutput)
+        self._jDecoderPanel.add(self._jLabelExample)
+        self._jDecoderPanel.add(self._jLabelOputFormat)
+        self._jDecoderPanel.add(self._jCheckBoxOutputFormat)
+        self._jDecoderPanel.add(self._jComboDecodeType)
+        self._jDecoderPanel.add(self._jScrollPaneIntput)
+        self._jDecoderPanel.add(self._jScrollPaneOutput)
+        self._jDecoderPanel.add(self._jButtonDecoder)
+
+        # Setup Tabs
+        self._jConfigTab = swing.JTabbedPane()
+        self._jConfigTab.addTab("Decoder", self._jDecoderPanel)
+        callbacks.customizeUiComponent(self._jConfigTab)
+        callbacks.addSuiteTab(self)
+        callbacks.registerContextMenuFactory(self)
+
+        return
+
+
+    def getTabCaption(self):
+        return 'MyDecoder'
+
+    def getUiComponent(self):
+        return self._jConfigTab
+
+
+    def createMenuItems(self, invocation):
+        menu = []
+
+        # Which part of the interface the user selects
+        ctx = invocation.getInvocationContext()
+
+        # Message Viewer Req will show menu item if selected by the user
+        if ctx == 0 or ctx == 2:
+          menu.append(swing.JMenuItem("Send to MyDecoder", None, actionPerformed=lambda x, inv=invocation: self.sendToMyDecoder(inv)))
+
+        return menu if menu else None
+
+    def sendToMyDecoder(self, invocation):
+        requestResponses = invocation.getSelectedMessages();
+        start = invocation.getSelectionBounds()[0];
+        end = invocation.getSelectionBounds()[1];
+        if end > start:
+            selected_content = self._helpers.bytesToString(requestResponses[0].getRequest()[start: end])
+            self._jTextAreaInputData.setText(selected_content)
+            parentTab = self._jConfigTab.getParent()
+
+            # task = self.highlightParentTab(parentTab, self._jConfigTab)
+            # thread.start_new_thread(self.highlightParentTab, (parentTab, self._jConfigTab,))
+            thread1 = self.HighlightParentTab(parentTab, self._jConfigTab)
+            thread1.start()
+
+
+    class HighlightParentTab(threading.Thread):
+        def __init__(self, parentTab, childComponent):
+            threading.Thread.__init__(self)
+            self.parentTab = parentTab
+            self.childComponent = childComponent
+
+        def run(self):
+            for i in range(self.parentTab.getTabCount()):
+                if self.parentTab.getComponentAt(i).equals(self.childComponent):
+                    self.parentTab.setBackgroundAt(i, Color(0xE58900))
+                    time.sleep(3)
+                    self.parentTab.setBackgroundAt(i, Color.BLACK)
+
+
+    def decode(self, button):
+        decodeType = self._jComboDecodeType.getSelectedIndex()
+        data = self._decodeTypeFunc[decodeType](self._jTextAreaInputData.getText())
+
+        self._jTextAreaOutputData.setText(data)
+
+
+    def change_decode(self, button):
+        decodeType = self._jComboDecodeType.getSelectedIndex()
+        data = self._decodeTypeFunc[decodeType]()
+
+        self._jLabelExample.setText(data)
+
+
+    def unicodeToStr(self, data=None):
+        # convert %u6E2C%u8A66 or \u6E2C\u8A66 to chinese
+        if data is None:
+            return r'Example: %u4E2D or \u4E2D to string'
+
+        split_chr = r'\u'
+        if re.search('^%u.*', data) is not None:
+            split_chr = '%u'
+
+        data = data.split(split_chr)[1:]
+        ret = ''
+        for c in data:
+            ret += unichr(int(c, 16))
+        return ret
+
+
+    def utf8ToStr(self, data=None):
+        # convert %E4%B8%AD to '中'
+        if data is None:
+            return r'Example: %e4%b8%ad or \xe4\xb8\xad to string'
+
+        split_chr = '%'
+        if re.search(r'^\\x.*', data) is not None:
+            split_chr = r'\x'
+        try:
+            byte = ''.join(data.split(split_chr)[1:])
+            byte = byte.decode("hex")
+            return byte.decode('utf-8')
+        except TypeError as e:
+            print e
+            return 'Error'
+
+
+    def strToUnicode(self, data=None):
+        if data is None:
+            return r'Example: String to %u6E2C%u8A66 format'
+        ret = ''
+        if self._jCheckBoxOutputFormat.isSelected():
+            ret = repr(data)[1:].replace("'", '')
+        else:
+            ret = repr(data)[1:].replace("'", '').replace(r'\u', '%u')
+
+        return ret
+
+
+    def strToUtf8(self, data=None):
+        if data is None:
+            return r'Example: String to %e4%b8%ad format'
+        ret = ''
+        if self._jCheckBoxOutputFormat.isSelected():
+            ret = repr(data)[1:].replace("'", '').replace(r'\x', '%')
+        else:
+            ret = repr(data)[1:].replace("'", '')
+
+        return ret
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # burp-MyDecoder
 Burp Suite Extension
+
+## Support
+
+* Unicode To string
+* UTF-8 to string
+* String to Unicode
+* String to UTF-8
+
+## Demo
+
+![image](https://github.com/Bu4275/burp-MyDecoder/blob/master/demo1.png)
+
+![image](https://github.com/Bu4275/burp-MyDecoder/blob/master/demo2.png)
